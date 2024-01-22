1aa6e556b5e3bfd409116fb3e9571c1a302a0b56
isaacjosh71@gmail.com <isaacjosh71@gmail.com>
2024-01-22 12:03:35 +0100

    ss

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ module.exports = {
             await User.findByIdAndUpdate(req.user.id, {$set: {skills: true}})
             res.status(200).json({status: true})
         } catch (error) {
-            res.status(500).json({error: error}) 
+            res.status(200).json({status: true})
         }
      },
 
