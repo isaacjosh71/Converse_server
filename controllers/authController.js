fb89e4e97091452465ffeba0b3be6ca6dd14c3c1
isaacjosh71@gmail.com <isaacjosh71@gmail.com>
2024-02-08 18:45:42 +0100

    v

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,7 +105,7 @@ module.exports = {
             }
 
         try {
-            await User.findIdAndUpdate(req.body.password,
+            await User.findOneAndUpdate(req.body.password,
                 {$set: CryptoJs.AES.encrypt(user.password, process.env.SECRET).toString()}, 
                 {new:true})
                 res.status(200).json({status: true})
